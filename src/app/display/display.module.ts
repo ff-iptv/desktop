79d150d20bebb29ae8a1475268dcdf1e86764633
Flavio Silva <flavio.silva@iatec.com>
2018-07-01 22:47:08 -0300

    Add icons

---
+++ src/app/display/display.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DisplayRoutingModule } from './display-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    DisplayRoutingModule
+  ],
+  declarations: []
+})
+export class DisplayModule { }
